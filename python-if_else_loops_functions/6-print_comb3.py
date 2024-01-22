74513f7d4a93ca8c0660175e6ec8d872751dffee
EderHodaj <eder0302@gmail.com>
2024-01-22 17:33:46 +0100

    is lower

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(0, 10):  # Corrected the range
+    print(i, end=", ")
+    for j in range(0, 10):  # Corrected the range
+        print(j, end=", ")
+    print()  # Added a newline after each inner loop
+	
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+	str = "abcdefghijklmnopqrstuvwxyz"
+	for letter in str:
+		if letter == c:
+			return True
+		else:
+			return False
