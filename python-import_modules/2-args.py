562a0ec55e1681a51d26d9905182c3a9a3298560
EderHodaj <eder0302@gmail.com>
2024-01-23 16:45:31 +0000

    Update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     n = len(sys.argv)
     argv = sys.argv
     if (n - 1) == 1:
-        print("1 argument")
+        print("1 argument:")
     elif n == 1:
         print("0 arguments.")
     else:
-        print(f"{len(sys.argv) - 1} arguments")
+        print(f"{len(sys.argv) - 1} arguments:")
 
     for index in range(1, n):
         print(f"{index}: {argv[index]}")
