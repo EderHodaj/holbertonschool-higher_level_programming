30defcaaa4cae8fe4fde6c2272a98b66197adf29
EderHodaj <eder0302@gmail.com>
2024-01-23 16:28:02 +0000

    Change

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print(f"{len(sys.argv) - 1} argumnets")
     n = len(sys.argv)
     argv = sys.argv
-    for index in range(1, n):
-        if argv[index]:
-            print(f"{index}: {argv[index]}")
-        else:
+    if (n - 1) == 1:
+        print(f"{len(sys.argv) - 1} argumnet")
+    else:
+        print(f"{len(sys.argv) - 1} argumnets")
+
+    for index in range(1,n):
+        if n ==  0:
             print(f"{index}.")
+            break
+        else:
+            print(f"{index}: {argv[index]}")
