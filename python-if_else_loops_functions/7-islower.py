be1667863031459c9f08d096efda7f861b0e2d0d
EderHodaj <eder0302@gmail.com>
2024-01-23 10:54:08 +0100

    changes

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for letter in c:
-        letter = ord(letter)
-        if letter >= 97 and letter <= 122:
-            return True
-        else:
-            return False
-
+	letter = ord(letter)
+	if letter >= 97 and letter <= 122:
+		return True
+	return False
