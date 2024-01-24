618e8e1fd03f253db266d4113111b9e7106579e1
EderHodaj <eder0302@gmail.com>
2024-01-24 08:30:54 +0000

    Update_dir

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for name in hidden_4:
-        if name[0] == "_" and name[1] == "_":
-            continue
-        else:
-            print(sorted(name))
+    import hidden_4
+    sentences = dir(hidden_4)
+    for name in sentences:
+        if name[0] != "_":
+            print(name)
