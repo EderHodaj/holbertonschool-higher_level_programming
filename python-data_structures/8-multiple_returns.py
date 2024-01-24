43b9f1f35b023c63380bc1767726515e05aba420
EderHodaj <eder0302@gmail.com>
2024-01-24 17:18:31 +0000

    tuple

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
-    new_tuple = (len(sentence), sentence[0])
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    new_tuple = (len(sentence), first_char)
     return new_tuple
