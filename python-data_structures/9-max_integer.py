661ef24d1b18bbf5c732ccd0877e8931759f9c45
EderHodaj <eder0302@gmail.com>
2024-01-24 17:39:31 +0000

    find max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    count = my_list[0]
     if len(my_list) == 0:
         return None
-    for element in my_list:
-        if element > count:
-            count = element
-    return count
+    else:
+        count = my_list[0]
+        for element in my_list:
+            if element > count:
+                count = element
+        return count
