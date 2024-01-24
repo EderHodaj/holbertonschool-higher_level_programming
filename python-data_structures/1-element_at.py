c9e33246964a09fd81b4be35994bd1965a110d0f
EderHodaj <eder0302@gmail.com>
2024-01-24 11:16:53 +0000

    element at index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx =< 0:
+    if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
-        return my_list[idx]
+        return (my_list[idx])
