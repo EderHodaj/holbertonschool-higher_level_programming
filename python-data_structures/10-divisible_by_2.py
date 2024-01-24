75e3080424359151b07838efa7196417e96ca12e
EderHodaj <eder0302@gmail.com>
2024-01-24 17:50:29 +0000

    /2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -9,11 +9,3 @@ def divisible_by_2(my_list=[]):
             new_list.append(False)
 
     return new_list
-
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
