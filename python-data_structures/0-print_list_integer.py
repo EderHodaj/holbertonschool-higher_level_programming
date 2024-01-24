2c22618bef12103cb7e042d636f34a9f894b7b70
EderHodaj <eder0302@gmail.com>
2024-01-24 10:57:35 +0000

    print numbers in list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for numbers in my_list:
-        print(f"{numbers}")
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+        print("{}".format(numbers))
