4c4696da2de552c13fa6bc21c567060c4b10cc5a
EderHodaj <eder0302@gmail.com>
2024-01-29 10:27:14 +0000

    replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -7,9 +7,3 @@ def search_replace(my_list, search, replace):
         else:
             new_lists.append(el)
     return new_lists
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
