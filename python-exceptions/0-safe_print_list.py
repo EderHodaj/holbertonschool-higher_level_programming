0c64386730cdd588e9343b491446a3d204f78590
EderHodaj <eder0302@gmail.com>
2024-01-30 11:20:25 +0000

    print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for index in range(0, x):
         try:
-            print(my_list[index], end = "")
+            print(my_list[index], end="")
             count += 1
         except IndexError:
             break
