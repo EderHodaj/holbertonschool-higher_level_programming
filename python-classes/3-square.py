fd6b22cf1f60215456d9cfef1396cd47335c3387
EderHodaj <eder0302@gmail.com>
2024-01-31 13:24:53 +0000

    Square_area

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+    We will create a class named Square
+'''
+
+
+class Square:
+    '''
+        Size must be an integer and greater than 0
+    '''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
