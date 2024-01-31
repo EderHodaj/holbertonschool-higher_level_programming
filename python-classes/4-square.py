7cd6ced22c366525387e196193a9d93d12a96303
EderHodaj <eder0302@gmail.com>
2024-01-31 13:33:56 +0000

    Getter_Setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,11 +9,19 @@ class Square:
         Size must be an integer and greater than 0
     '''
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-    
+        self.__size = value
+
     def area(self):
         return self.__size ** 2
