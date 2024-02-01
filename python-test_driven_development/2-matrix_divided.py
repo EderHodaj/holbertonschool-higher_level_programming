8fb5c40c563fdba29df1d9c9256526623de5953b
EderHodaj <eder0302@gmail.com>
2024-02-01 18:27:51 +0000

    Matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    for row in matrix:
+        if not all(isinstance(elem, (int, float)) for elem in row):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    count = len(matrix[0])
+    for i in matrix:
+        if count != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    new_matrix = []  # Initialize as an empty list, not [[]]
+    for row in matrix:
+        new_row = []
+        for i in row:
+            new_row.append(round(i / div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
