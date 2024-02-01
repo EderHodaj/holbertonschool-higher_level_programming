6280aef4d5517445bdfabe06ecfb9c86eb9657d2
EderHodaj <eder0302@gmail.com>
2024-02-01 22:16:58 +0000

    Text_indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,15 +10,17 @@ def text_indentation(text):
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    start_of_line = True
+
+    start_of_line = True  # To track whether we are at the beginning of a line
 
     for char in text:
         if start_of_line:
             print(char, end="")
             start_of_line = False
         else:
-            if char in ".?:":
-                print("", end="\n\n")
-                start_of_line = True
+            if char in ".?:":  # Check if the character is '.', '?', or ':'
+                print("", end=" ")  # Add a space after the punctuation
+                print("", end="\n\n")  # Print two newlines
+                start_of_line = True  # Next character will be at the beginning of a line
             else:
                 print(char, end="")
