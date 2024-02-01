058b48fb978cdad62d6763f7c1a88294cd376ce1
EderHodaj <eder0302@gmail.com>
2024-02-01 22:05:18 +0000

    Text_indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Testing excercises
+'''
+
+
+def text_indentation(text):
+    '''
+    Function that prints text with two newlines
+    '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        print(i, end="")
+        if i == "." or i == "?" or i == ":":
+            print("", end="\n\n")
+            
+    
