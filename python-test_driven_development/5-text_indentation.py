09147286dea5adeeecacdb89b286fbafb209014c
EderHodaj <eder0302@gmail.com>
2024-02-02 10:49:05 +0000

    Text_ind

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,7 +10,13 @@ def text_indentation(text):
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in text:
-        print(i, end="")
-        if i == "." or i == "?" or i == ":":
-            print("", end="\n\n")
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ['.', '?', ':']:
+            print("\n\n", end='')
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
