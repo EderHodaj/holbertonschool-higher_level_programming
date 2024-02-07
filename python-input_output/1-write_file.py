81bb4f2b3a6b97969b1f5b06f443d33cda2b4482
EderHodaj <eder0302@gmail.com>
2024-02-07 19:36:42 +0000

    Write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,9 +5,9 @@ Input output
 
 
 def write_file(filename="", text=""):
-     '''
+    '''
     Write files
     '''
     with open(filename, 'w', encoding="utf-8") as my_file:
-        contetnt = my_file.write(text)
+        content = my_file.write(text)
     return len(text)
