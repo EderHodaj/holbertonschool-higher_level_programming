74a536fddaf6ca29ae0ada4f0214eb3282a3ce43
EderHodaj <eder0302@gmail.com>
2024-02-07 19:25:31 +0000

    Write_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+'''
+Input output
+'''
+
 
 def read_file(filename=""):
+    '''
+    Read files
+    '''
     with open(filename, encoding="utf-8") as f:
         print(f.read())
