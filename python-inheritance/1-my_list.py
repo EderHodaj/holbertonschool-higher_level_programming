25f04bb1639b9243f7d46a4802103d57c8812e56
EderHodaj <eder0302@gmail.com>
2024-02-07 19:52:28 +0000

    Sorted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,4 +9,16 @@ class MyList(list):
     Functions
     '''
     def print_sorted(self):
+        sorted_list = sorted(self)
         print(sorted(self))
+        return sorted_list
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
