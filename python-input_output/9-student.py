a67ee503258ddaf1c28a4826c7384d4c3c0e3c20
EderHodaj <eder0302@gmail.com>
2024-02-09 09:37:51 +0000

    self.dict

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+'''
+Input output
+'''
+
+
 class Student:
+    '''
+    Create class with function
+    '''
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
