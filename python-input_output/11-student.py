050c3eec972b8ee1f0d8659ebd8b79abbb7d3d2c
EderHodaj <eder0302@gmail.com>
2024-02-09 10:12:50 +0000

    Reload_json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for class_to_json"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for key in attrs:
+            if key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+        return new_dict
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.__dict__[key] = json[key]
