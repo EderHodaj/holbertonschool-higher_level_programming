f19cf9a5ff3df083b11073929d197474b18525a9
EderHodaj <eder0302@gmail.com>
2024-02-12 19:27:58 +0000

    str_method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,6 @@ class Rectangle(Base):
         '''Print Rectangle Instance'''
         for i in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
