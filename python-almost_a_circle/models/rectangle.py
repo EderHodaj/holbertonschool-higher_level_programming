0d607aa6960f01d5979095a102a371ee97938b3d
EderHodaj <eder0302@gmail.com>
2024-02-12 13:53:25 +0000

    Rectangle_class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,24 +20,31 @@ class Rectangle(Base):
         @property
         def width(self):
             return self.__width
+
         @width.setter
         def width(self, value):
             self.__width = value
+
         @property
         def height(self):
             return self.__height
+
         @height.setter
         def height(self, value):
             self.__height = value
+
         @property
         def x(self):
             return self.__x
+
         @x.setter
         def x(self, value):
             self.__x = value
+
         @property
         def y(self):
             return self.__y
+
         @y.setter
         def y(self, value):
             self.__y = value
