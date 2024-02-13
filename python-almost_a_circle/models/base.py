01344a6c298c69b6109320d71be2f7d6c9317b33
EderHodaj <eder0302@gmail.com>
2024-02-13 11:39:56 +0000

    dict_to_json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,6 +18,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''Convert to json'''
         if list_dictionaries is None:
