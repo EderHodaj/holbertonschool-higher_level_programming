c7d917bb13b2130547635ebf848ea6ec6ec1b637
EderHodaj <eder0302@gmail.com>
2024-02-13 10:52:49 +0000

    Getter_setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,3 +12,13 @@ class Square(Rectangle):
     def __str__(self):
         '''str'''
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        '''size of this square'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
