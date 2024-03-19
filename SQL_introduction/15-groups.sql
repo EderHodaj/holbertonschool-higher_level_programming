5de4d0b74b3d3ed86b876651b9e7e3b0dbd7a313
EderHodaj <eder0302@gmail.com>
2024-03-19 19:06:19 +0100

    update

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- Number by score
+SELECT score, COUNT(*) as 'number' FROM second_table
+GROUP BY score
+ORDER BY number DESC;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- Say my name
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
