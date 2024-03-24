da039394088fc2298b48d1ab1a8afe8ab2fa5cff
EderHodaj <eder0302@gmail.com>
2024-03-24 16:57:25 +0100

    new

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,43 @@
-#!/usr/bin/python3
-"""List all states from the database hbtn_0e_0_usa"""
-
 import MySQLdb
-from sys import argv
+import sys
+
+def list_states(username, password, database):
+    # Connect to MySQL database
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    # Execute SQL query to select all states
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetch all rows
+    states = cursor.fetchall()
+
+    # Print the results
+    for state in states:
+        print(state)
+
+    # Close cursor and connection
+    cursor.close()
+    connection.close()
 
 if __name__ == "__main__":
-    """List all states from the database hbtn_0e_0_usa"""
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Check if correct number of arguments provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    # Get MySQL credentials from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the function to list states
+    list_states(username, password, database)
